a39dbbae83f27c145f71a8ce76b543c8167791af
Konstantin Bernik <bernik.konstantin@yandex.ru>
2024-05-07 04:26:15 +0500

    first version

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src.masks import mask_account_number, mask_card_number
+from src.widget import mask_account_card, get_date
 
-print(mask_card_number("7000792289606361"))
-print(mask_account_number("73654108430135874305"))
+print(mask_account_card("Maestro 1596837868705199"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(get_date("2018-07-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -2,4 +2,20 @@ from src.masks import mask_account_number, mask_card_number
 
 
 def mask_account_card(number: str) -> str:
-    """Функция прнимает строку и маскирует номер карты или счета"""
+    """Функция принимает строку и маскирует номер карты или счета"""
+    if len(number.split()[-1]) == 16:
+        mask_card = mask_card_number(number.split()[-1])
+        result = f"{number[:-16]}{mask_card}"
+    elif len(number.split()[-1]) == 20:
+        mask_account = mask_account_number(number.split()[-1])
+        result = f"{number[: -20]}{mask_account}"
+
+    return result
+
+
+def get_date(date: str) -> str:
+    """Функүиә принимает строку записи даты и форматирует ее"""
+    date_list = date[0:10].split("-")
+    new_date = ".".join(date_list[::-1])
+
+    return new_date
