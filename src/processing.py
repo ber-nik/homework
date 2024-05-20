5fc7e803d11cef70b05b4c17a7dc84ae6d274f4f
Konstantin Bernik <bernik.konstantin@yandex.ru>
2024-05-20 14:18:15 +0500

    fix_test_processig_2

--- src/processing.py
+++ src/processing.py
@@ -9,9 +9,9 @@ def filter_by_state(dicts_list: list, state: str = "EXECUTED") -> list:
     return filtred_dict
 
 
-def sorted_by_date(dicst_list: list, reverse: bool = True) -> list:
+def sorted_by_date(dicst_list: list, direction: bool = True) -> list:
     """Функция принимает на вход список словарей и возвращает новый список, в котором исходные словари
     отсортированы по убыванию даты (ключ date)."""
-    sorted_dicts = sorted(dicst_list, key=lambda x: x['date'], reverse=reverse)
+    sorted_dicts = sorted(dicst_list, key=lambda x: x['date'], reverse=direction)
     return sorted_dicts
 
--- test/test_processing.py
+++ test/test_processing.py
@@ -2,114 +2,39 @@ import pytest
 from src.processing import filter_by_state, sorted_by_date
 
 
-@pytest.mark.parametrize("dicts_list", [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ])
-def test_filter_by_state(dicts_list, filtred_dict):
-    assert filter_by_state(dicts_list) == filtred_dict
+@pytest.mark.parametrize(
+    "result_list, state",
+    [
+        ([
+             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+         ], "EXECUTED"),
+        ([
+             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+         ], "CANCELED")
+    ]
+)
+def test_filter_by_state(dicst_list, result_list, state):
+    assert filter_by_state(dicst_list, state=state) == result_list
 
 
 @pytest.mark.parametrize(
-    "dicst_list, direction, sorted_list",
+    "direction, sorted_list",
     [
-        (
-            [
-                {
-                    "id": 41428829,
-                    "state": "EXECUTED",
-                    "date": "2019-07-03T18:35:29.512364",
-                },
-                {
-                    "id": 939719570,
-                    "state": "EXECUTED",
-                    "date": "2018-06-30T02:08:58.425572",
-                },
-                {
-                    "id": 594226727,
-                    "state": "CANCELED",
-                    "date": "2018-09-12T21:27:25.241689",
-                },
-                {
-                    "id": 615064591,
-                    "state": "CANCELED",
-                    "date": "2018-10-14T08:21:33.419441",
-                },
-            ],
-            True,
-            [
-                {
-                    "id": 41428829,
-                    "state": "EXECUTED",
-                    "date": "2019-07-03T18:35:29.512364",
-                },
-                {
-                    "id": 615064591,
-                    "state": "CANCELED",
-                    "date": "2018-10-14T08:21:33.419441",
-                },
-                {
-                    "id": 594226727,
-                    "state": "CANCELED",
-                    "date": "2018-09-12T21:27:25.241689",
-                },
-                {
-                    "id": 939719570,
-                    "state": "EXECUTED",
-                    "date": "2018-06-30T02:08:58.425572",
-                },
-            ],
-        ),
-        (
-            [
-                {
-                    "id": 41428829,
-                    "state": "EXECUTED",
-                    "date": "2019-07-03T18:35:29.512364",
-                },
-                {
-                    "id": 939719570,
-                    "state": "EXECUTED",
-                    "date": "2018-06-30T02:08:58.425572",
-                },
-                {
-                    "id": 594226727,
-                    "state": "CANCELED",
-                    "date": "2018-09-12T21:27:25.241689",
-                },
-                {
-                    "id": 615064591,
-                    "state": "CANCELED",
-                    "date": "2018-10-14T08:21:33.419441",
-                },
-            ],
-            False,
-            [
-                {
-                    "id": 939719570,
-                    "state": "EXECUTED",
-                    "date": "2018-06-30T02:08:58.425572",
-                },
-                {
-                    "id": 594226727,
-                    "state": "CANCELED",
-                    "date": "2018-09-12T21:27:25.241689",
-                },
-                {
-                    "id": 615064591,
-                    "state": "CANCELED",
-                    "date": "2018-10-14T08:21:33.419441",
-                },
-                {
-                    "id": 41428829,
-                    "state": "EXECUTED",
-                    "date": "2019-07-03T18:35:29.512364",
-                },
-            ],
-        ),
-    ],
+        (True, [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        ]),
+        (False, [
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        ])
+    ]
 )
-def sorted_by_date(dicst_list, direction, sorted_list):
+def test_sorted_by_date(dicst_list, direction, sorted_list):
     assert sorted_by_date(dicst_list, direction) == sorted_list
