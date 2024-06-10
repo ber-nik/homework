55c8608e9d58ee62024d67cd2d85929bdb3c2002
Konstantin Bernik <bernik.konstantin@yandex.ru>
2024-06-10 18:22:50 +0500

    created function external.py

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,16 @@
 import json
-import os
 
-def get_jcon_transactions(file_path: list[dict]) -> list[dict] or []:
+
+def get_jcon_transactions(file_path: str) -> list[dict] or []:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными
     о финансовых транзакциях."""
     try:
-        with open(file_path, 'r', encoding='utf-8') as f:
-            return json.loads(f.read())
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return json.loads(file.read())
     except:
         return []
 
-file_path = r'C:\Users\ЗС\work\tmp\homework\data\operations.json'
-transactions = get_jcon_transactions(file_path)
-print(transactions)
+if __name__ == "__main__":
+    file_path = r'C:\Users\ЗС\work\tmp\homework\data\operations.json'
+    transactions = get_jcon_transactions(file_path)
+    print(transactions)
