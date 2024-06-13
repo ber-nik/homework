3b1fb21e349cfb4d11b2c313782d0b5e748854de
Konstantin Bernik <bernik.konstantin@yandex.ru>
2024-06-13 19:38:23 +0500

    added logers for moduls: utils and masks

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,18 @@
+import logging
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("logs/ masks.log")
+file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
+
+
 def mask_card_number(card: str) -> str:
     """функция принимает номер карты и возвращает его зашифрованным"""
     masked_number = f"{card[:4]} {card[4:6]}** **** {card[-4:]}"
+
+    logger.info(f"Получаем зашифрованный номер карты {masked_number}")
     return masked_number
 
 
