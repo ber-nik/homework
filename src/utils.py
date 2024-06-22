ca89bf993cd0a80a5df430050529db0fa97867eb
Konstantin Bernik <bernik.konstantin@yandex.ru>
2024-06-22 17:34:14 +0500

    created new func and tests for its

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,11 @@
 import json
 from json import JSONDecodeError
 import logging
+import pandas as pd
 
 logger = logging.getLogger("utils")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("logs/ utils.log")
+file_handler = logging.FileHandler("logs/utils.log")
 file_formater = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
@@ -30,3 +31,13 @@ if __name__ == "__main__":
     file_path = r"C:\Users\ЗС\work\tmp\homework\data\operations.json"
     transactions = get_json_transactions(file_path)
     print(transactions)
+
+
+def read_csv(file_path):
+    """Считывает данные из CSV файла и возвращает DataFrame."""
+    return pd.read_csv(file_path)
+
+
+def read_xlsx(file_path):
+    """Считывает данные из XLSX файла и возвращает DataFrame."""
+    return pd.read_excel(file_path, engine="openpyxl")
--- test/test_utils.py
+++ test/test_utils.py
@@ -1,8 +1,9 @@
 import unittest
 from json.decoder import JSONDecodeError
 from unittest.mock import mock_open, patch
+import pandas as pd
 
-from src.utils import get_json_transactions
+from src.utils import get_json_transactions, read_csv, read_xlsx
 
 
 class TestGetJsonTransactions(unittest.TestCase):
@@ -23,6 +24,24 @@ class TestGetJsonTransactions(unittest.TestCase):
         self.assertEqual(result, [])
         mock_open.assert_called_once_with("dummy_path.json", "r", encoding="utf-8")
         mock_json_loads.assert_called_once()
+
+
+@patch("pandas.read_csv")
+def test_read_csv(mock_read_csv):
+    """Тестирование функции чтения CSV файла."""
+    # Здесь мы подменяем вызов pandas.read_csv нашим моком
+    mock_read_csv.return_value = pd.DataFrame({"data": [1, 2, 3]})
+    df = read_csv("dummy.csv")
+    assert not df.empty
+
+
+@patch("pandas.read_excel")
+def test_read_xlsx(mock_read_excel):
+    """Тестирование функции чтения XLSX файла."""
+    # Аналогично подменяем вызов pandas.read_excel
+    mock_read_excel.return_value = pd.DataFrame({"data": [1, 2, 3]})
+    df = read_xlsx("dummy.xlsx")
+    assert not df.empty
 
 
 if __name__ == "__main__":
